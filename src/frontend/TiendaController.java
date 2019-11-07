66139b7f2a68be46bced5a274c412b170dd6cbe5
Nahuel <nahubtiel@gmail.com>
2019-11-07 20:05:59 -0300

    Terminada la ventana Tienda y comenzada la ventana Carrito

--- src/frontend/TiendaController.java
+++ src/frontend/TiendaController.java
@@ -5,9 +5,17 @@
  */
 package frontend;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -19,9 +27,23 @@ public class TiendaController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    VBox vbox;
+    @FXML
+    AnchorPane pane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        HBox h = new HBox();
+        h.setSpacing(20);
+        h.getChildren().add(new ProductoController());
+        vbox.getChildren().add(h);
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        ProductoController prod = new ProductoController();
+        vbox.getChildren().add(prod);
+    }
+
 }
